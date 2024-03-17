27ac1877414e2a09d6dcccf9f49394526071dc8d
LuisaFC <luisabsb@gmail.com>
2024-03-17 17:16:38 -0300

    downcating comment

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -16,6 +16,7 @@ public class ChessMatch {
         //percorrer a matriz de peças do tabuleiro e para cada peça será feito um downcasting para ChessPiece
         for(int i=0; i< board.getRows(); i++){
             for(int j=0; j< board.getColumns(); j++){
+                //downcasting pois board retorna Piece(superclasse das peças do jogo) e as peças do jogo de xadrez são do tipo ChessPiece(subclasse)
                 mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
